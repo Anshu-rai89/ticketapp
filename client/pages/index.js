dc00efb7e96bd48882a140d397a867f88dba302a
Anshu Rai <raianshu8991@gmail.com>
2021-04-05 16:52:33 +0530

    adding auth check

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,15 @@
-export default ()=>{
-    return <h1>Landing page</h1>
-}
+import buildClient from "../apis/buildClient";
+
+const LandingPage = ({ currentUser }) => {
+  console.log("current user", currentUser);
+  return currentUser?<h1>Welcome {currentUser.email}</h1>:<h1>Please SignIn To Continue</h1>;
+};
+
+LandingPage.getInitialProps = async (context) => {
+  const client = buildClient(context);
+  const { data } = await client.get("/api/users/currentUser");
+
+  return data;
+};
+
+export default LandingPage;
